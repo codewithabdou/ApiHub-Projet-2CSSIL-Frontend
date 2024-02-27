45a487158c10ec9dec807eaca685a78d3f48c74d
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2024-02-27 22:10:40 +0100

    fix button import in ui library

--- app/components/ui/carousel.tsx
+++ app/components/ui/carousel.tsx
@@ -7,7 +7,7 @@ import useEmblaCarousel, {
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/app/[locale]/components/ui/button"
+import { Button } from "@/app/components/ui/button"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
