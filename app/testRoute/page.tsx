8c49921ad271ceb107bdcef4277acb2a765b087a
Yanis02 <ka_djouima@esi.dz>
2024-03-01 00:35:29 +0100

    added testemonies section and footer to the home page

--- app/testRoute/page.tsx
+++ app/testRoute/page.tsx
@@ -1,9 +1,18 @@
+import Footer from "@app/components/home/Footer";
+import Testemonies from "@app/components/home/Testemonies";
 import React from "react";
 
 // this is a test page to test the components you are working on
 
 const TestPage = () => {
-  return <div>TestPage</div>;
+  return( <div>
+    
+    <Testemonies/>
+    <Testemonies/>
+
+
+    <Footer/>
+  </div>)
 };
 
 export default TestPage;
--- config/images.ts
+++ config/images.ts
@@ -1,1 +1,8 @@
-const IMAGES = {};
+const IMAGES = {
+    Instagram : "images/Instagram.png",
+    Facebook : "images/Facebook.png",
+    Twitter : "images/TwitterX.png",
+    LinkedIn : "images/LinkedIn.png"
+
+};
+export default IMAGES
