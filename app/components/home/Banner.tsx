7c40f588a263ae263de9c16923505bdcee02ca4d
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2024-03-01 16:30:45 +0100

    fix banner

--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import Image from 'next/image';
-import { IMAGES } from '@config';
-import { Button } from '../ui/button';
+import React from "react";
+import Image from "next/image";
+import { IMAGES } from "@config";
+import { Button } from "../ui/button";
 
 const Banner = () => {
   return (
-    <div className=" w-full px-10  flex py-8 flex-col md:flex-row bg-primary" style={{backgroundImage: `url(${IMAGES.BANNER[0].bannerbg})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-      <div className="container mx-12 my-12 font-bold flex flex-col justify-center  md:items-start md:w-1/2">
-        <p className="mb-4 md:mb-0 text-3xl md:text-center md:text-4xl   text-white  md:text-left">Commencez à enrichir vos projets :<br/> découvrez nos APIs dès maintenant</p>
-        <Button className='bg-secondary text-base my-8 md:my-10 w-56 md:w-60 font-bold'>Commencez</Button>
+    <div
+      className=" w-full relative bg-primary bg-opacity-80 px-4 py-8 gap-8 md:max-h-96 justify-around  flex md:px-[10%] flex-col md:flex-row "
+      style={{
+        backgroundImage: `url(${IMAGES.BANNER[0].bannerbg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundBlendMode: "soft-light",
+      }}
+    >
+      <div className="order-2 z-10 md:order-1 w-full  font-extrabold flex flex-col  items-center  md:items-start ">
+        <p className=" text-2xl md:text-start max-w-[30ch] text-center md:text-3xl   text-white  ">
+          Commencez à enrichir vos projets : découvrez nos APIs dès maintenant
+        </p>
+        <Button className="bg-secondary  hover:border-white hover:border-[1px] hover:scale-110 text-base my-8 md:my-10 w-56 md:w-60 font-bold">
+          Commencez
+        </Button>
       </div>
-      <div className="flex p-12    justify-center md:justify-end md:w-1/2">
-        <Image   className='transform px-5 skew-y-3 hover:-translate-y-1 hover:scale-105 ' src={IMAGES.BANNER[0].banner} width={500} height={300} alt="Banner" />
+      <div className="flex  order-1 md:order-2  justify-center ">
+        <Image
+          className=" object-contain scale-110 "
+          src={IMAGES.BANNER[0].banner}
+          width={291}
+          height={385}
+          alt="Banner"
+        />
       </div>
     </div>
   );
--- app/components/home/index.ts
+++ app/components/home/index.ts
@@ -1,2 +1,4 @@
 export { default as Footer } from "./Footer";
 export { default as Testemonies } from "./Testemonies";
+export { default as Nosservices } from "./Nosservices";
+export { default as Banner } from "./Banner";
