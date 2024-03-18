6df05c924b3d63cc34ba7f6b2b392af0d98a6eb5
liliabkm <liliaboukhima@gmail.com>
2024-03-18 21:09:42 +0100

    Contactez nous

--- app/testRoute/page.tsx
+++ app/testRoute/page.tsx
@@ -16,8 +16,8 @@ const TestPage = () => {
     <div className="flex min-h-screen flex-col items-center justify-evenly  p-4 lg:p-[10%]">
 
 
-      <ContactUs/>
        <CreateCategoryForm />
+       <ContactUs/>
 
      <CreateApiForm/>
 
