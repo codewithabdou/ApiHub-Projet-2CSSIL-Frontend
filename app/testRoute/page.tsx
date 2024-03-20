2e2ba8e8658092bd4531d956741c586c1ca6bcfd
liliabkm <liliaboukhima@gmail.com>
2024-03-20 10:55:39 +0100

    SuppliersDataTable

--- app/testRoute/page.tsx
+++ app/testRoute/page.tsx
@@ -1,17 +1,29 @@
 
 import ContactUs from "@app/components/home/ContactUs";
+import AdminUsersDataTable from "@app/components/home/SupplierManagement.tsx/data-table";
 
 
 // this is a test page to test the components you are working on
 const TestPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-evenly  p-4 lg:p-[10%]">
-
        <ContactUs/>
 
 
     </div>
   );
+
+
+
+
+
+
+
+
+
+
+
+  
 };
 
 export default TestPage;
