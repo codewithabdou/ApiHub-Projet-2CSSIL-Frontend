87d5698fb5af6a2f3ef7e3f8d99d83e3f2371a4e
ilyes benabbes <ki_benabbes@esi.dz>
2024-03-22 12:16:45 +0100

    added the integration of the api , modified the getapis function , modified the route authorization function , commenting the filters

--- app/testRoute/page.tsx
+++ app/testRoute/page.tsx
@@ -1,16 +1,11 @@
-
-import DetailedCategory from "@app/admin/categories/details/page";
+import DetailedCategory from "@app/admin/categories/[categoryId]/page";
 import ContactUs from "@app/components/home/ContactUs";
-
 
 // this is a test page to test the components you are working on
 const TestPage = () => {
   return (
     <div className="flex min-h-screen flex-col p-4">
-
-       <DetailedCategory id={1}/>
-
-
+      <DetailedCategory params={{categoryId:1}} />
     </div>
   );
 };
