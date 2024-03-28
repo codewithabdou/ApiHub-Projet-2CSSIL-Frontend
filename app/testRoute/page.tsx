7f95410ae9fd8a22c87668353c6d6755afbb3dbf
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2024-03-28 14:30:57 +0100

    fix

--- app/testRoute/page.tsx
+++ app/testRoute/page.tsx
@@ -1,27 +1,12 @@
 import DetailedCategory from "@app/admin/categories/[categoryId]/page";
-import ContactUs from "@app/components/home/ContactUs";
-import AdminUsersDataTable from "@app/components/home/SupplierManagement.tsx/data-table";
 
 // this is a test page to test the components you are working on
 const TestPage = () => {
   return (
-
     <div className="flex min-h-screen flex-col p-4">
       <DetailedCategory params={{ categoryId: 1 }} searchParams={{}} />
     </div>
   );
-
-
-
-
-
-
-
-
-
-
-
-  
 };
 
 export default TestPage;
