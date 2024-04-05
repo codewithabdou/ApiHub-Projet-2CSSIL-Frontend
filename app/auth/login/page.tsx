0924137c110b6faffa01825c7e09e37e5d01627e
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2024-04-05 14:05:46 +0200

    fix

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@app/components/Shared/Landing page layout/Navbar";
+import NavbarUserActions from "@app/components/Shared/Landing page layout/NavbarUserActions";
 import LoginForm from "@app/components/auth/LoginForm";
 
 export default function LoginPage() {
   return (
     <>
-      <Navbar />
+      <NavbarUserActions />
       <main className="flex min-h-screen flex-col items-center justify-evenly  p-4 lg:p-[10%]">
         <LoginForm />
       </main>
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@app/components/Shared/Landing page layout/Navbar";
+import NavbarUserActions from "@app/components/Shared/Landing page layout/NavbarUserActions";
 import RegisterForm from "@app/components/auth/RegisterForm";
 
 export default function LoginPage() {
   return (
     <>
-      <Navbar />
+      <NavbarUserActions />
       <main className="flex min-h-screen flex-col items-center justify-evenly p-4 lg:p-[10%]">
         <RegisterForm />
       </main>
